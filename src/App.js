d2658f975f0bd9d9eb209ee6d66bfc94dcfdb260
bigmanik <igbinomwanhia10@gmail.com>
2023-02-13 02:15:29 -0800

    first stage in setting up image uploader

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+
 import './App.css';
+import axios from 'axios';
+import React, { useState } from 'react';
 
 function App() {
+  const [userInfo, setuserInfo] = useState({file:[],
+  });
+  const handleInputChange = (event) => {
+    setuserInfo({
+      ...userInfo,
+      file:event.target.files[0],
+    })
+  }
+  const submit = async () =>{
+        const formdata = new FormData()
+        formdata.append('avatar', userInfo.file);
+        axios.post("http://http://localhost:3000/imageupload", formdata,{
+          headers: { "Content-Type": "multipart/form-data"}
+        })
+        .then(res => {console.warn(res);
+        })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h3 className='text-w'>upload your image</h3>
+      <div className='formdesign'>
+        <div className='form-row'>
+          <label className='text-w'>Select Image:</label>
+          <input type="file" className='form-c'
+          name = "upload" onChange={handleInputChange}/>
+        </div>
+        <div className='form-row'>
+          <button type= "submit" className='btn btn-blue' onClick={()=>submit()}>save</button>
+        </div>
+      </div>
+     
+      
     </div>
   );
 }
